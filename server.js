52b33272077e8434ee3b3c5b99ed50390b5de5e9
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-27 16:23:52 +0200

    Update API endpoint and enhance product descriptions in server.js

--- server.js
+++ server.js
@@ -7,57 +7,62 @@ Configuriamo gli asset statici sull’applicazione in modo che si possano visual
 Testare su postman
 */
 
+
 //Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
 //creo le costanti per richiamare express
 const express = require('express');
 const app = express();
 const port = 3030;
 
+
 //con app.use rendo richiamabili le immagini all'intenro di public
 app.use(express.static('public'));
 
-//faccio app.listen
-app.listen(port, () => {
-    console.log(`Server is listening on port http://localhost:${port}`);
-});
 
 //app.get per home
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to the Bakery</h1>');
 });
 
+
 //app.get per products
-app.get('/api/Products', (req, res) => {
+app.get('/api/posts', (req, res) => {
     const postsProducts = [
         {
             title: 'Ciambellone ',
-            content: 'Content of Post 1',
+            content: 'Ciambellone bianco con zucchero a velo',
             image: '/images/ciambellone.jpeg',
             tags: ['dolce', 'dessert', 'zucchero a velo']
         },
         {
             title: 'Cracker di barbabietola',
-            content: 'Content of Post 2',
+            content: 'Cracker di barbabietola con semi vari',
             image: '/images/cracker_barbabietola.jpeg',
             tags: ['dolce', 'semi', 'barbabietola']
         },
         {
             title: 'Pane fritto dolce',
-            content: 'Content of Post 3',
+            content: 'Pane fritto dolce tagliato a tozzetti',
             image: '/images/pane_fritto_dolce.jpeg',
             tags: ['fritto', 'pane', 'zucchero']
         },
         {
-            title: 'Pasta alla barbabietola',
-            content: 'Content of Post 4',
+            title: 'Pasta di barbabietola',
+            content: 'Pasta di barbabietola con pistacchio',
             image: '/images/pasta_barbabietola.jpeg',
             tags: ['pasta', 'pistacchio', 'barbabietola']
         },
         {
             title: 'Torta paesana',
-            content: 'Content of Post 5',
+            content: 'Torta paesana xon noci e limone',
             image: '/images/torta_paesana.jpeg',
             tags: ['torta', 'limone', 'noci']
         }];
     res.json(postsProducts);
+});
+
+
+//faccio app.listen
+app.listen(port, () => {
+    console.log(`Server is listening on port http://localhost:${port}`);
 });
